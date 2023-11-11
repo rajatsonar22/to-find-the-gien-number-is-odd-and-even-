c7f1864df50b8b5de62c0258ff4ec829e164e1f7
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2023-11-11 12:55:04 +0530

    Add files via upload

---
+++ to check odd or even.py
@@ -0,0 +1,7 @@
+#to fund is the givwen number is odd or even
+
+n =int(input("enter a value "))
+if( n % 2 == 0):
+    print("the given number is even ")
+else:
+    print("the given value is odd ")
